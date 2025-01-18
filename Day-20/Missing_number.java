20411f3c2dc24197fb209df97ca7e9b2e064616f
Nithish3115 <nithish031105@gmail.com>
2025-01-18 19:39:52 +0530

    Brute Force

---
+++ Day-20/Missing_number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+       int sum1=0;
+        int sum2=0;
+
+        for(int i=0; i<=nums.length; i++){
+            sum1=sum1+i;
+        }
+
+        for(int i=0; i<nums.length; i++){
+            sum2=sum2+nums[i];
+        }
+
+        return sum1-sum2;
+
+    }
+}
